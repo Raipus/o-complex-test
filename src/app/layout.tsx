2d446e1973f2f197b275ee41c7f8b9648a9a14b8
PC Raipus <nikita18.907@gmail.com>
2025-06-19 16:07:49 +0300

    Обновлены метаданные и шрифт глобальной обертки приложения

---
+++ src/app/layout.tsx
@@ -0,0 +1,25 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({
+  variable: "--font-inter",
+  weight: "400",
+});
+
+export const metadata: Metadata = {
+  title: "o-complex test",
+  description: "Тестовое задание by Иванов Никита",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={`${inter.variable} antialiased`}>{children}</body>
+    </html>
+  );
+}
